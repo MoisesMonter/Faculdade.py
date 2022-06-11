ff6d2fe9e3192621f0446d9ed8c4b9fe3531e374
Moisés Alexandre Monteiro Araujo <97893793+MoisesMonter@users.noreply.github.com>
2022-06-11 17:45:34 -0300

    Add files via upload

---
+++ c#/variaveis_e_primitivos.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Run
+{
+    class go
+    {
+        static void Main(string[] args)
+        {
+            string  var0 = "This is ";
+            bool    var1 = true;
+            string  var2 = ".\nI have this Year> ";
+            int     var3 = 24;
+ 
+            Console.WriteLine(var0+var1+var2+var3);
+
+        }
+    }
+}
