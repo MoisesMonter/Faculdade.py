af7b0514a0b9ad805cc01d3baba3b2e5494c8fbe
Moisés Alexandre Monteiro Araujo <97893793+MoisesMonter@users.noreply.github.com>
2022-06-11 15:37:48 -0300

    Add files via upload

---
+++ c#/hello.cs
@@ -0,0 +1,12 @@
+//Programa Hello, World!
+
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World")
+        }
+    }
+}
