db9ae754d64eae506555ad8a21512bf5800bdc2f
Moisés Alexandre Monteiro Araujo <97893793+MoisesMonter@users.noreply.github.com>
2022-12-28 17:18:50 +0000

    "fim deque array"

--- deque/lista_simples.py
+++ deque/lista_simples.py
@@ -16,8 +16,23 @@ def Incrementar(deque,bool):
         deque.insert(0,front)
     else:
         rear = int(input("Digite valor de entrada:"))
-        deque[int(len(deque)-1)]=(rear)
+        if len(deque) <=0:
+            deque.append(rear)
+        else:
+            deque.extend([rear])
+            
     return deque
+
+def Decrementar(deque,bool):
+    if bool == True:
+        if len(deque) <=0:
+            print("lsita vazia")
+        else:
+            deque.pop(0)
+    else:
+        deque.pop()
+    return deque
+
 
 if __name__ == "__main__":
     deque = []
@@ -26,12 +41,13 @@ if __name__ == "__main__":
         Limpar(plataforma)
         print(deque)
         x = int(input("0 - Sair\n1 - Incrementar Inicio\n2- Incrementar Final\n3 Deletar Inicio\n4Deletar Final\n\nSelecione:"))
-
         if x == 0:
             break
         elif x ==1:
             deque = Incrementar(deque,True)
         elif x ==2:
             deque = Incrementar(deque,False)
-
-        
+        elif x == 3:
+            deque = Decrementar(deque,True)
+        elif x == 4:
+            deque = Decrementar(deque,False)
